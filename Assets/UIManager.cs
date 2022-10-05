47d63466f5f202f21348ca2925681f3b11e7662c
david <53330739+davidagito@users.noreply.github.com>
2022-10-06 00:19:32 +0800

    working on ui manager

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Inventory")]
     [SerializeField]
-    private Canvas InventoryCanvas;
+    private Canvas InventoryScreen;
+
+    [Space]
+    [Header("Market")]
     [SerializeField]
-    private Canvas MarketScreenCanvas;
-    [SerializeField]
-    private Canvas ItemPreviewScreen;
+    private Canvas MarketScreen;
+
+    public void ToggleInventoryScreen(bool isActive)
+    {
+        ToggleScreen(InventoryScreen, isActive);
+    }
+
+    public void ToggleMarketScreen(bool isActive)
+    {
+        ToggleScreen(MarketScreen, isActive);
+    }
+
+    private void ToggleScreen(Canvas canvas, bool isActive)
+    {
+        canvas.enabled = isActive;
+    }
 }
